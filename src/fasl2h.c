3f0e1397ac40ad9c5d0abcd6e484fbc9a81b9714
Alex Queiroz <asandroq@gmail.com>
2009-09-10 16:27:26 +0000

    * Added fasl2h tool

---
+++ src/fasl2h.c
@@ -0,0 +1,68 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int ret;
+  unsigned value;
+  FILE *in, *out;
+  char iname[512], oname[512];
+
+  if(argc != 2) {
+    fprintf(stderr, "Usage: %s <basename>\n", argv[0]);
+    exit(1);
+  }
+
+  snprintf(iname, 511, "%s.fasl", argv[1]);
+  iname[511] = '\0';
+  snprintf(oname, 511, "%s.h", argv[1]);
+  oname[511] = '\0';
+
+  in  = fopen(iname, "r");
+  if(!in) {
+    fprintf(stderr, "Could not open file %s for reading.\n", iname);
+    exit(1);
+  }
+  
+  out = fopen(oname, "w");
+  if(!out) {
+    fclose(in);
+    fprintf(stderr, "Could not open file %s for writing.\n", oname);
+    exit(1);
+  }
+
+  fprintf(out, "/*\n");
+  fprintf(out, "\tThis code was automatically generated from a .fasl file.\n");
+  fprintf(out, "\tDo not edit it.\n");
+  fprintf(out, "*/\n\n");
+
+  fprintf(out, "static const uint8_t %s_buf[] = {\n", argv[1]);
+
+  /* bytecode beginning */
+  ret = fscanf(in, " #(");
+  if(ret == EOF) {
+    fclose(in);
+    fclose(out);
+    fprintf(stderr, "invalid input file\n");
+    exit(1);
+  }
+
+  while(1) {
+    ret = fscanf(in, " %u", &value);
+    if(ret == 0) {
+      break;
+    } else if(ret == EOF) {
+      fclose(in);
+      fclose(out);
+      fprintf(stderr, "unexpected end of input\n");
+      exit(1);
+    }
+
+    fprintf(out, "%u, ", value);
+  }
+
+  fprintf(out, "}\n");
+
+  return 0;
+}
+

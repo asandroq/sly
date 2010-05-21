ee7a4bcaddecf222f81428f84c4a8cb6ef06a40b
Alex Sandro Queiroz e Silva <alex.queiroz@myfreecomm.com.br>
2010-05-21 17:24:03 -0300

    * Assigned complex variables marked as such * Body of LAMBDA must be a single expression

--- tests/test3.scm
+++ tests/test3.scm
@@ -4,10 +4,11 @@
       (define bleh (+ 123 (car '(3 . 4))))
       (define blih (- 456 (cdr '(6 . 7))))
       (define bloh (call/cc (lambda (k) (cons k 45))))
+      (define blooh (not blah))
       (define (fact-iter n a)
         (set! blih a)
         (if (and (= n blah)
-                 (not bloh))
+                 (or bloh blooh))
             a
             (fact-iter (- n 1) (* a n))))
       (fact-iter n 1))
